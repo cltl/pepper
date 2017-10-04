aebadc635107f7093fc79f4d667446dbcfdf8f7f
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2017-10-04 17:08:03 +0200

    add more hardcoded items + sentences

--- pepper/apps/game.py
+++ pepper/apps/game.py
@@ -3,8 +3,6 @@ import sys; sys.path.append("..")
 from pepper.app import App
 from pepper.event import ObjectPresentEvent, GestureDetectedEvent
 
-from pepper.image.camera import PepperCamera
-from pepper.image.classify import ClassifyClient
 from pepper.knowledge.wordnet import WordNet
 
 from threading import Thread, Timer
@@ -12,16 +10,18 @@ from time import sleep
 from random import choice
 
 
-INTRODUCTION = """
-Hello everybody, my name is Pepper! I am a social humanoid robot.
-This means I can interact with you through seeing, hearing, talking and moving.
-However, in order to do anything - I need to be programmed.
-Remember, a robot is only as smart as you make it to be!
+# INTRODUCTION = """
+# Hello everybody, my name is Pepper! I am a social humanoid robot.
+# This means I can interact with you through seeing, hearing, talking and moving.
+# However, in order to do anything - I need to be programmed.
+# Remember, a robot is only as smart as you make it to be!
+#
+# Today, we will play a game. This is an opportunity for me to show off all my skills.
+# I will think of an object I see and give you hints to guess what I am thinking about.
+# I am excited to see how fast you can guess!
+# """
 
-Today, we will play a game. This is an opportunity for me to show off all my skills.
-I will think of an object I see and give you hints to guess what I am thinking about.
-I am excited to see how fast you can guess!
-"""
+INTRODUCTION = """I'm skipping the intro, whahaha."""
 
 
 OBJECT_HINTS = {
@@ -49,6 +49,16 @@ OBJECT_HINTS = {
         "You can use this object in summer",
         "You wear it on your head",
         "You don't use it indoors"
+    ],
+    'Christmas stocking': [
+        "This is a holiday item",
+        "You can wear it on your feet",
+        "It has stripes"
+    ],
+    'stole': [
+        "You wear it when it is cold",
+        "It goes around your neck",
+        "this object is usually made from wool"
     ]
 }
 
@@ -57,15 +67,25 @@ OBJECT_COLORS = {
     'banana': 'yellow',
     'coffee mug': 'black',
     'water jug': 'white',
-    'sunglasses': 'black'
+    'sunglasses': 'black',
+    'Christmas stocking': 'red and white',
+    'stole': 'red and black'
 }
 
 TEXT_SUCCESS = [
-    "Congratulations, I was indeed looking for a {}. How very good of you"
+    "Congratulations, I was indeed looking for a {}. How very good of you",
+    "Good job. it is indeed a {}",
+    "Awesome, {} it is!",
+    "You beat me! You can be proud of yourself by calling {}!",
+    "{}! " * 3
 ]
 
 TEXT_FAILURE = [
-    "You are showing me a {}. This is, sadly, not what I was looking for"
+    "You are showing me a {}. This is, sadly, not what I was looking for",
+    "Nope, it's not a {}. you wanna try again?",
+    "It might look like a {}. But guess what! It's not!",
+    "{}, whahaha, I think not!",
+    "No, No, No No, not a {}!"
 ]
 
 ANIMATION_FAILURE = [
@@ -96,15 +116,15 @@ class Game(App):
         self.speech = self.session.service("ALAnimatedSpeech")
 
         # ..:: Introduction ::.. #
+        print("Introduction")
         self.speech.say(INTRODUCTION)
 
         # ..:: Runtime Variables ::.. #
-        self.recognised_objects = set()
         self.current_object = ""
 
         # ..:: Subscribe to Events ::.. #
         self.events.append(GestureDetectedEvent(self.session, self.on_gesture))
-        self.events.append(ObjectPresentEvent(self.session, self.on_present, "PresentCamera"))
+        self.events.append(ObjectPresentEvent(self.session, self.on_present, "PresentCamera2"))
 
         # Start Hinting
         hint_thread = Thread(target=self.hint)
@@ -113,34 +133,37 @@ class Game(App):
 
     def on_gesture(self, gesture_name):
         self.speech.say("Let's Play!")
-
         self.current_object = choice(OBJECT_COLORS.keys())
-        hint = OBJECT_COLORS[self.current_object] if self.current_object in OBJECT_COLORS else \
-            WordNet.definitions(self.current_object)['noun'][0]
+        color = OBJECT_COLORS[self.current_object]
+        self.speech.say("I spy with my little eye... something which is... {}".format(color))
 
-        self.speech.say("I spy with my little eye something which is {}".format(
-            hint))
+        print("Start Game: '{}'".format(self.current_object))
 
-    def on_present(self, object_score, object):
+    def on_present(self, object_score, objects):
         if self.current_object:
             if object_score > 0.6:
-                if self.current_object in object:
+                if self.current_object in objects:
                     animation = choice(ANIMATION_SUCCESS)
                     self.speech.say("^start({}){}^wait({})".format(
                         animation, choice(TEXT_SUCCESS).format(self.current_object), animation))
+
+                    print("Success")
                     self.current_object = ""
 
                 else:
                     animation = choice(ANIMATION_FAILURE)
                     self.speech.say("^start({}){}^wait({})".format(
-                        animation, choice(TEXT_FAILURE).format(choice(object)), animation
+                        animation, choice(TEXT_FAILURE).format(choice(objects)), animation
                     ))
+                    print("Failure: '{}'".format(objects))
 
     def hint(self):
         while True:
             if self.current_object in OBJECT_HINTS:
-                self.speech.say("I have a hint! {}".format(choice(OBJECT_HINTS[self.current_object])))
-            sleep(15)
+                hint = choice(OBJECT_HINTS[self.current_object])
+                self.speech.say("I have a hint! ... {}".format(hint))
+                print("Hint: '{}'".format(hint))
+            sleep(20)
 
 if __name__ == "__main__":
     app = Game(["192.168.1.100", 9559])
