82f08b8c0fc666b2c30f814adc10c7a0e7b846f7
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2017-11-01 16:00:38 +0100

    add VU bachelors info

---
+++ pepper/knowledge/vu_bachelors.py
@@ -0,0 +1,142 @@
+SUMMARY = {
+    "Communication and Information Studies":
+        [
+            "There are two Bachelors in the Communication and Information Studies:"
+            "1. Language and Media."
+            "2. Language Learning and Language Teaching."
+        ],
+
+    "Language and Media":
+        [
+            "The Bachelor-programme Language and Media offers you a unique opportunity to combine the study of "
+            "language and communication processes with new and exciting developments in media use, "
+            "against a backbone of generally accepted theories and methodologies.",
+
+            "In the first year you are introduced to linguistics, communication science and discourse analysis."
+            "You learn how communication processes interact with media,"
+            "and explore how media are used in organizational contexts."
+            "Of course, we train your general academic skills, such as research methodologies and academic writing.",
+
+            "After the first year, you have a solid base from which to explore various aspects of language in media."
+            "We also offer theoretical courses on intercultural communication and global English."
+            "You will then apply these theoretical insights in more practice-oriented courses on "
+            "multilingual communication and language management.",
+        
+            "Third Year. "
+            "In the first semester, you will take elective courses or gain valuable experience by working as an intern."
+            "In the second semester, you will complete your specialization and conduct your own research project, "
+            "which you will report on in your Bachelor thesis."],
+
+    "Language Learning and Language Teaching":
+        [
+            "Language Learning and Language Teaching bridges the gap between what linguists know about the "
+            "learnability of languages and what language teachers experience in daily life."
+            "We aim to explain why certain aspects of languages are extremely difficult to learn while others are not,"
+            "and how this varies for different learner groups."
+            "We use this information to critically analyze teaching and assessment materials and to "
+            "develop and investigate new teaching methods and language interventions.",
+
+            "First of all, the first year gives you a broad theoretical base reflecting the current state of the art "
+            "regarding the structure of language, how language interacts with cognition and what kind of factors "
+            "are driving language acquisition. Second, you will learn the basics of adjacent research fields, "
+            "such as communication studies and discourse analysis."
+            "Last but not least, you will work on some indispensable general academic skills, "
+            "such as research methodologies and academic writing.",
+
+            "In the second year, your knowledge of language learning will be further developed as we dive deeper"
+            "into a number of linguistic domains, such as phonetics, morphology, semantics. "
+            "This will help you become more proficient in recognizing typical and atypical language patterns "
+            "in both monolingual and bilingual children. You will also be introduced to the field of language teaching "
+            "and you will learn and experience how language teaching may influence "
+            "second and foreign language learning, for instance in the module 'second language learning'."
+            "We also help you to further develop your academic skills, in the courses 'statistics' and 'philosophy'.",
+
+            "In the first semester of the third year of this Bachelors programme, you will choose a minor programme."
+            "You are completely free in your choice, depending predominantly on your future plans and your personal "
+            "interests. You can choose from minor programmes such as Brain and Mind, European History and Culture, "
+            "Digital Humanities, and many others. In the second semester of this year, "
+            "you will dot the i's and cross the t's by focusing on language teaching, "
+            "dyslexia and on assessing language proficiency. "
+            "You will round off your programme by writing a Bachelor's thesis."
+        ],
+
+    "History and International Studies":
+        [
+            "History and International Studies is a three year Bachelor programme."
+            "It starts with a broad based first year that covers the history from antiquity until the present, "
+            "followed by a year in which students choose a specialization, "
+            "and finishes with a thesis on a subject of one's choice in Year 3.",
+
+            "In Year 1, students take courses on the history of antiquity, the medieval period, early modern history "
+            "and modern and contemporary history. Throughout this first year European developments are studied "
+            "in relationship with Middle Eastern, Asian, American, and African developments. "
+            "Students are introduced to the field and approaches of global history and taught to reflect "
+            "on the relationship between history and contemporary debates on issues such as "
+            "democracy and authoritarianism, global inequality, or climate change."
+            "From Year 1, students are also trained in Academic English as well as in research and writing skills. ",
+
+            "In Year 2, students choose two units of a modern language."
+            "They also take mandatory courses on history and theory, international relations, global political economy,"
+            "and on the methods of the historical and social sciences."
+            "Students learn to apply these methods to analyzing interactions between individuals,"
+            "states, societies and international organizations. They are also offered a range of electives, organized "
+            "in four tracks: Politics & Economy, Migration & Ethnicity, Culture & Religion, and Heritage & Memory. ",
+
+            "In Year 3, students can opt to take a semester abroad, complete an internship or choose a minor."
+            "The final semester includes courses on international relations in a historical perspective, an elective,"
+            "and the writing of a thesis, which is supervised on an individual basis by our staff members."
+         ],
+
+    "Literature and Society":
+        [
+            "The Bachelor-program Literature and Society (English) offers a unique opportunity to combine classic "
+            "literature study with two cutting-edge specializations. You will read canonical British works,"
+            "such as William Shakespeare, Jane Austen, Charles Dickens, Oscar Wilde, en Virginia Woolf."
+            "But also explore contemporary novels from different English-speaking countries. "
+            "In our courses, you will analyse these texts from two cutting-edge perspectives: "
+            "English literature in a changing world, and English literature in a visual culture.",
+
+            "In the first year you are introduced to literary analysis and literary theory."
+            "You learn how to read a Bronte novel as well as a film version of a Shakespeare play."
+            "We train your general academic skills and also kindle your creative writing practice.",
+
+            "After the first year, you have a solid base from which to explore the relations between literature,"
+            "culture and society. We take you from the Renaissance to the twenty-first century,"
+            "reading canonical English works of literature as well as new and lesser known genres such as "
+            "travel literature, graphic novels and gothic genres.",
+
+            "You become a specialist either in 'English literature in a changing world' or "
+            "'English literature in a visual culture' and conduct your own research project, "
+            "resulting in your BA thesis."
+        ],
+
+    "Philosophy":
+        [
+            "Studying philosophy at VU Amsterdam calls for active participation, intense study and great ambition. "
+            "We foster all of this in working seminars, in which we guide you in the pursuit of knowledge. "
+            "You are expected to adopt an independent attitude towards learning in general, "
+            "to participate actively in group meetings, and to prepare well for lectures. ",
+
+            "During the first year, you take part in foundational courses in logic, epistemology, metaphysics, "
+            "and ethics, and also in introductory courses in the history of philosophy: general surveys of ancient, "
+            "medieval, modern and contemporary philosophical traditions will provide you with the basis for expanding "
+            "your knowledge: you will acquire a broad understanding of the field of philosophy and its methodology, "
+            "and of the various research questions that define that field.",
+
+            "In the second year you will both revise and refine your knowledge: you will attend courses in ethics,"
+            "philosophy of science, and philosophy of mind, and you'll be working more in-depth; "
+            "some special courses will be devoted to the reading and analysis of classical philosophical texts, "
+            "others will concern Arabic philosophy and diversity in philosophy. "
+            "What has the Greek philosophical tradition given rise to in contexts other than the Western one?",
+
+            "In the first semester of the third year you can follow a minor programme of your own choice, "
+            "which allows you to deepen your knowledge of a specific philosophical subject or to get acquainted "
+            "with another discipline. The final semester will be devoted to writing your Bachelor thesis. "
+            "You choose a subject that you find of particular interest, in consultation with your supervisor. "
+            "In this final stage, you will apply the knowledge you acquired and the research skills you learnt so far, "
+            "to produce an original work in which you develop your own philosophical views."
+        ],
+}
+
+
+print(SUMMARY)
