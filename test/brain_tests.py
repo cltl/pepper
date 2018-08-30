d3c097fd390b00341650dde9d69c0cb81b4152ec
Selene Baez <sel.bs.93@hotmail.com>
2018-08-30 13:05:41 +0200

    sample for scenario learn objects

--- test/brain_tests.py
+++ test/brain_tests.py
@@ -1,4 +1,5 @@
 import json
+from datetime import date
 
 from pepper.brain.long_term_memory import LongTermMemory
 from pepper.brain.utils.base_cases import statements, questions, experiences, visuals
@@ -9,12 +10,35 @@ if __name__ == "__main__":
     brain = LongTermMemory()
 
     # Re import base
-    for stat in statements:
-        brain.update(stat)
+    # for stat in statements:
+    #     brain.update(stat)
+    #
+    # brain.experience(experiences[0])
 
-    brain.experience(experiences[0])
+    type = brain.process_visual('Apple')
 
-    brain.process_visual('fruit')
+    capsule = {  # Leolani saw an apple
+        "subject": {
+            "label": "",
+            "type": ""
+        },
+        "predicate": {
+            "type": ""
+        },
+        "object": {
+            "label": "apple",
+            "type": type
+        },
+        "author": "front_camera",
+        "chat": None,
+        "turn": None,
+        "position": "0-15-0-15",
+        "date": date.today()
+    }
+
+    brain.experience(capsule)
+    print(brain.get_classes())
+
     brain.process_visual('chair')
     brain.process_visual('Red')
 
