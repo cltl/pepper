2366e3b5572b0c1881798d9cc38306b61d8eb1c2
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-02-28 16:07:47 +0100

    automate ntlk packages download

--- pepper/language/__init__.py
+++ pepper/language/__init__.py
@@ -1,2 +1,18 @@
 from .process_utterance import analyze_utterance, classify_and_process_utterance, reply_to_question, reply_to_statement
 from .language import *
+
+
+# Download NLTK Dependencies
+import nltk
+
+# Make sure Averaged Perceptron Tagger has been downloaded
+try: nltk.data.find('taggers/averaged_perceptron_tagger.zip')
+except: nltk.download('averaged_perceptron_tagger')
+
+# Make sure Wordnet has been downloaded
+try: nltk.data.find('corpora/wordnet.zip')
+except: nltk.download('wordnet')
+
+# Make sure Punkt has been downloaded
+try: nltk.data.find('tokenizers/punkt.zip')
+except: nltk.download('punkt')
