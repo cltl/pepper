9c04d00e77698d57e11fbaf375fddf570791c219
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-05-12 17:09:04 +0200

    continue to search web after unknown question

--- pepper/responder/brain.py
+++ pepper/responder/brain.py
@@ -44,9 +44,12 @@ class BrainResponder(Responder):
                 self._log.debug("REPLY: {}".format(reply))
 
                 if (isinstance(reply, str) or isinstance(reply, unicode)) and reply != "":
-                    # Return Score and Response
-                    # Make sure to not execute the response here, but just to return the response function
-                    return 1.0, lambda: app.say(re.sub(r"[\s+_]", " ", reply))
+                    if "but I do not know this case" in reply:
+                        app.say(reply + ". But, let me try and search the web!")
+                    else:
+                        # Return Score and Response
+                        # Make sure to not execute the response here, but just to return the response function
+                        return 1.0, lambda: app.say(re.sub(r"[\s+_]", " ", reply))
 
         except Exception as e:
             self._log.error(e)
