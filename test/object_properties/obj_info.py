b0519b4862a2609f124b749b7a275cd5a081677b
SuzanaB <suz.basic@gmail.com>
2020-01-28 14:50:28 +0100

    remove depth

--- test/object_properties/obj_info.py
+++ test/object_properties/obj_info.py
@@ -1,62 +1,71 @@
-from classes import ObjectInstance
-from util import read_dir, read_object_properties, clustering, dominant_cluster, stats, color_mapping
+from util import clean_color_name, detect_objects, dominant_cluster, stats, color_mapping
 
 from matplotlib import colors as mcolors
 from matplotlib import pyplot as plt
+from PIL import Image
 from datetime import datetime
 
+import numpy as np
+
 import os
-import re
+import json
 import sqlite3
 
 
 ROOT = './data/20190930_133826'
-# ROOT = './data/small'
 OBJ_HANDLE = "_obj.json"
 RGB_HANDLE = "_rgb.png"
 DEPTH_HANDLE = "_depth.npy"
 META_HANDLE = "_meta.json"
 
 
-def map_colors():
+def read_dir(root):
     """
-    Translate matplotlib colors from hex to rgb and edit color names
-    :return: dictionary mapping from 1049 matplotlib color names to normalized rgb values
+    Read the directory with generated images, objects and associated metadata.
+    :param root: directory path
+    :return: images and objects in the directory (AbstractImage, list)
     """
 
-    #   TODO: change dict to {value: name}, check the number of colors
+    images = [item for item in os.listdir(root) if os.path.isdir(os.path.join(root, item))]
+
+    for image in images:
+        img_path = os.path.join(root, image)
+        objects = [os.path.join(img_path, obj) for obj in os.listdir(img_path)]
+
+        yield img_path, objects
+
+
+def read_object_properties(obj_path):
+    """
+    Retrieve object data and metadata.
+    :param obj_path: object path
+    :return: object data and metadata
+    """
+
+    with open(os.path.join(obj_path, META_HANDLE[1:])) as meta_file:
+        meta_info = json.load(meta_file)
+
+    obj_type = meta_info['name']
+    obj_confidence = meta_info['confidence']
+    obj_bounds = meta_info['bounds']
+    obj_img = Image.open(os.path.join(obj_path, RGB_HANDLE[1:]))
+    obj_rgb = np.array(obj_img).astype('float')
+
+    return obj_type, obj_confidence, obj_bounds, obj_rgb, obj_img
+
+
+def map_colors(keywords):
+    """
+    Translate matplotlib colors from hex to rgb
+    :return: dictionary mapping from normalized rgb values to 1099 cleaned matplotlib color names
+    """
 
     rgb_mapping = dict()
 
     for name, value in dict(mcolors.get_named_colors_mapping()).items():
         rgb_value = mcolors.to_rgb(value)
-        if name.startswith('xkcd'):
-            name = name[5:]
-        if name.startswith('tab'):
-            name = name[4:]
-        if ' urple' in name:
-            name = name.replace(' urple', ' purple')
-        if 'lavendar' in name:
-            name = name.replace('lavendar', 'lavender')
-        if 'gray' in name:
-            name = name.replace('gray', 'grey')
-        # TODO: additional tokenization: check CSS keywords
-        d = re.compile('dark[a-z]+')
-        f = d.search(name)
-        if f:
-            if f.group() not in ('darkish', 'darker'):
-                name = re.sub('dark', 'dark ', name)
-        l = re.compile('light[a-z]+')
-        f = l.search(name)
-        if f:
-            if f.group() not in ('lightish', 'lighter'):
-                name = re.sub('light', 'light ', name)
-        m = re.compile('medium[a-z]+')
-        f = m.search(name)
-        if f:
-            name = re.sub('medium', 'medium ', name)
-        name = name.replace(' ', '_')
-        rgb_mapping[name] = rgb_value
+        color_name = clean_color_name(name, keywords)
+        rgb_mapping[rgb_value] = color_name
 
     return rgb_mapping
 
@@ -68,88 +77,116 @@ def save_types(obj, rgb_mapping):
     :param rgb_mapping:
     :return:
     """
-    obj_type, obj_confidence, obj_bounds, obj_depth, obj_rgb, obj_img = read_object_properties(obj)
+    obj_type, obj_confidence, obj_bounds, obj_rgb, obj_img = read_object_properties(obj)
     if obj_confidence < 0.5 or obj_type == 'person':
         return 'id', 'features'
     else:
-        obj_instance = ObjectInstance(os.path.basename(obj), obj_type, obj_confidence, obj_bounds,
-                                      obj_depth, obj_rgb, obj_img)
-        result, clusters = clustering(obj_instance.rgb, obj_instance.depth, obj_instance.img)
+        result, clusters = detect_objects(obj_rgb, obj_img)
         dom_cluster = dominant_cluster(result, clusters)
-        dom_result = filter(lambda res: res[1] == dom_cluster, result)
+        dom_result = [res for res in result if res[1] == dom_cluster]
         avg_triplet, mode_triplet, median_triplet = stats(dom_result)
-        obj_instance.features = avg_triplet + mode_triplet + median_triplet
-        obj_instance.color = color_mapping(avg_triplet, rgb_mapping)
-        print('{} {}'.format(obj_instance.color, obj_instance.type))
+        obj_features = avg_triplet + mode_triplet + median_triplet
+        obj_color = color_mapping(avg_triplet, rgb_mapping)
+        obj_id = os.path.basename(obj)
+        print('{} {}'.format(obj_color, obj_type))
 
-        if not os.path.exists(os.path.join('./results', obj_instance.type)):
-            os.mkdir(os.path.join('./results', obj_instance.type))
-        plt.savefig('./results/{}/{}_{}_{}'.format(obj_instance.type,
-                                                   obj_instance.color, obj_instance.type, obj_instance.id))
+        if not os.path.exists(os.path.join('./results', obj_type)):
+            os.makedirs(os.path.join('./results', obj_type))
+        plt.savefig('./results/{}/{}'.format(obj_type, obj_id))
 
-        return obj_instance.id, obj_instance.features
+        return obj_id, obj_features, obj_color
 
 
-def total_count():
+def total_count(paths):
     """
 
     :return:
     """
-    img = len([(img_path, objects) for img_path, objects in read_dir(ROOT)])
-    obj = len([obj for img_path, objects in read_dir(ROOT) for obj in objects])
+    img = len([(img_path, objects) for img_path, objects in read_dir(ROOT) if any(obj not in paths for obj in objects)])
+    obj = len([obj for img_path, objects in read_dir(ROOT) for obj in objects if obj not in paths])
 
     return img, obj
 
 
-def read_table(c):
+def read_color_mapping(c):
+
+    c.execute('SELECT * FROM rgb_mapping')
+    mapping = dict()
+    for row in c:
+        mapping[eval(row[0])] = row[1]
+
+    return mapping
+
+
+def create_color_mapping(conn, c):
+
+    color_keywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive',
+                      'purple', 'red', 'silver', 'teal', 'white', 'yellow', 'grey', 'brown', 'orange', 'pink',
+                      'turquoise', 'lavender', 'magenta']
+
+    mapping = map_colors(color_keywords)
+
+    c.execute('CREATE TABLE rgb_mapping(rgb TEXT, name TEXT);')
+    for key, value in mapping.items():
+        c.execute('INSERT INTO rgb_mapping VALUES (?, ?);', (str(key), str(value)))
+    conn.commit()
+
+    return mapping
+
+
+def read_object_info(c):
     c.execute('SELECT path FROM object_info')
-    paths = c.fetchall()
+    paths = []
+    for row in c:
+        paths.append(str(row[0]))
 
     return paths
 
 
-def create_table(c):
-    c.execute('CREATE TABLE object_info(id TEXT, path TEXT, features TEXT);')
+def create_object_info(conn, c):
+    c.execute('CREATE TABLE object_info(id TEXT, path TEXT, features TEXT, color TEXT);')
+    conn.commit()
 
 
 def main():
 
+    total_start = datetime.now()
     conn = sqlite3.connect('instances.db')
-    c = conn.cursor()
+    cur = conn.cursor()
     try:
-        known_paths = read_table(c)
+        rgb_mapping = read_color_mapping(cur)
     except sqlite3.OperationalError:
-        create_table(c)
+        rgb_mapping = create_color_mapping(conn, cur)
+    try:
+        known_paths = read_object_info(cur)
+    except sqlite3.OperationalError:
+        create_object_info(conn, cur)
         known_paths = []
 
-    total_start = datetime.now()
-
-    rgb_mapping = map_colors()
-
-    total_img, total_obj = total_count()
+    total_img, total_obj = total_count(known_paths)
     img_count = 0
     obj_count = 0
 
     for image, objects in read_dir(ROOT):
-        img_count += 1
-        for obj_path in objects:
+        unknown_objects = [obj for obj in objects if obj not in known_paths]
+        if len(unknown_objects) > 0:
+            img_count += 1
+        for obj_path in unknown_objects:
             obj_count += 1
             obj_start = datetime.now()
             print('Processing image {}/{}, object {}/{}.'.format(img_count, total_img, obj_count, total_obj))
             print(obj_path)
-            if obj_path in known_paths:
-                print('Object already processed.')
+            obj_id, obj_features, obj_color = save_types(obj_path, rgb_mapping)
+            if obj_id == 'id':
+                print('Object could not be processed.')
             else:
-                obj_id, obj_features = save_types(obj_path, rgb_mapping)
-                if obj_id == 'id':
-                    print('Object could not be processed.')
-                else:
-                    c.execute('INSERT INTO object_info VALUES (?, ?, ?);', (str(obj_id), str(obj_path), str(obj_features)))
-                    conn.commit()
-                    obj_end = datetime.now()
-                    print('Object processing time: {}'.format(obj_end - obj_start))
-                    print('Total elapsed time: {}'.format(obj_end - total_start))
-                    print('\n')
+                cur.execute('INSERT INTO object_info VALUES (?, ?, ?, ?);',
+                            (str(obj_id), str(obj_path), str(obj_features), str(obj_color)))
+                conn.commit()
+                obj_end = datetime.now()
+                print('Object processing time: {}'.format(obj_end - obj_start))
+                print('Total elapsed time: {}'.format(obj_end - total_start))
+                print('\n')
 
     conn.close()
 
--- test/object_properties/util.py
+++ test/object_properties/util.py
@@ -1,17 +1,13 @@
-from pepper.framework import AbstractImage, Bounds
-
 from sklearn.cluster import DBSCAN
-from matplotlib import colors as mcolors
 from scipy.sparse import csr_matrix
-from PIL import Image
 from statistics import mean, mode, median
 
 import numpy as np
 import matplotlib.pyplot as plt
-import statistics
 
-import os
-import json
+import statistics
+import re
+
 
 OBJ_HANDLE = "_obj.json"
 RGB_HANDLE = "_rgb.png"
@@ -19,64 +15,84 @@ DEPTH_HANDLE = "_depth.npy"
 META_HANDLE = "_meta.json"
 
 
-def get_depth(path, rgb):
+def clean_color_name(name, keywords):
     """
-    Read image depth from file or default to zeros if no depth (for images generated from the laptop).
-    :param path: image path
-    :param rgb: image rgb
+    Clean matplotlib color names
+    :param name:
+    :param keywords:
+    :param vocab:
     :return:
     """
+    name = name.replace('/', ' ')
 
-    if os.path.exists(path + DEPTH_HANDLE):
-        depth = np.load(os.path.join(path + DEPTH_HANDLE))
-    else:
-        depth = np.zeros(rgb.shape[:2])
+    if name.startswith('xkcd'):
+        name = name[5:]
+    if name.startswith('tab'):
+        name = name[4:]
+    if name.startswith('urple'):
+        name = name.replace('urple', 'purple')
+    if ' urple' in name:
+        name = name.replace(' urple', ' purple')
+    if 'purpleish' in name:
+        name = name.replace('purpleish', 'purplish')
+    if 'lavendar' in name:
+        name = name.replace('lavendar', 'lavender')
 
-    return depth
+    left = search_left(name, 'dark', ('darkish', 'darker'))
+    if left:
+        name = left
+
+    left = search_left(name, 'light', ('lightish', 'lighter'))
+    if left:
+        name = left
+
+    for mod in ('medium', 'deep', 'hot', 'pale'):
+        left = search_left(name, mod)
+        if left:
+            name = left
+
+    # Add a space between two colors or color and modifier
+    for keyword in keywords:
+        left = search_left(name, keyword, (keyword + 'ish', keyword + 'y', 'aquamarine', 'blueberry', 'reddy', 'reddish'))
+        if left:
+            name = left
+        right = search_right(name, keyword)
+        if right:
+            name = right
+
+    # TODO: add this step for working with embeddings
+    '''for token in name.split(' '):
+        if token not in vocab:
+            if token.endswith('ish'):
+                new_token = token[:-3]
+                name = name.replace(token, new_token)
+            elif token.endswith('y'):
+                new_token = token[:-1]
+                name = name.replace(token, new_token)'''
+
+    return name
 
 
-def read_dir(root):
-    """
-    Read the directory with generated images, objects and associated metadata.
-    :param root: directory path
-    :return: images and objects in the directory (AbstractImage, list)
-    """
+def search_left(color_name, color_keyword, word_list=()):
 
-    images = filter(lambda item: os.path.isdir(os.path.join(root, item)), os.listdir(root))
-
-    for image in images:
-        img_path = os.path.join(root, image)
-        with open(img_path + META_HANDLE) as meta_file:
-            img_meta = json.load(meta_file)
-        img_rgb = np.array(Image.open(os.path.join(root, image + RGB_HANDLE)))
-        img_depth = get_depth(img_path, img_rgb)
-        img = AbstractImage(img_rgb, Bounds.from_json(img_meta["bounds"]), img_depth, img_meta["time"])
-        objects = [os.path.join(img_path, obj) for obj in os.listdir(img_path)]
-
-        yield img, objects
+    l = re.compile('{}[a-z]+'.format(color_keyword))
+    lf = l.search(color_name)
+    if lf:
+        if lf.group() not in word_list:
+            new_name = color_name.replace(color_keyword, color_keyword + ' ')
+            return new_name
 
 
-def read_object_properties(obj_path):
-    """
-    Retrieve object data and metadata.
-    :param obj_path: object path
-    :return: object data and metadata
-    """
+def search_right(color_name, color_keyword):
 
-    with open(os.path.join(obj_path, META_HANDLE[1:])) as meta_file:
-        meta_info = json.load(meta_file)
-
-    obj_type = meta_info['name']
-    obj_confidence = meta_info['confidence']
-    obj_bounds = meta_info['bounds']
-    obj_img = Image.open(os.path.join(obj_path, RGB_HANDLE[1:]))
-    obj_rgb = np.array(obj_img).astype('float')
-    obj_depth = get_depth(obj_path, obj_rgb)
-
-    return obj_type, obj_confidence, obj_bounds, obj_depth, obj_rgb, obj_img
+    r = re.compile('[a-z]+{}'.format(color_keyword))
+    rf = r.search(color_name)
+    if rf:
+        new_name = color_name.replace(color_keyword, ' ' + color_keyword)
+        return new_name
 
 
-def clustering(rgb, obj_depth, img):
+def detect_objects(rgb, img):
     """
     Identify clusters within an object bounding box based on color, position and depth.
     :param data: object rgb data
@@ -85,20 +101,17 @@ def clustering(rgb, obj_depth, img):
     :return: pixel-level features and cluster labels, set of identified clusters
     """
 
-    rgbd = np.dstack((rgb, obj_depth))
-
-    # TODO: double-check indices
-    new_rgbd = csr_matrix(rgbd.reshape(-1, 4))
+    new_rgb = csr_matrix(rgb.reshape(-1, 3))
 
     dbscan = DBSCAN(eps=5, min_samples=50)
-    db = dbscan.fit(new_rgbd)
+    db = dbscan.fit(new_rgb)
 
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
 
     labels = db.labels_
 
-    rows, cols, chs = rgbd.shape
+    rows, cols, chs = rgb.shape
 
     plt.figure(2)
     plt.subplot(2, 1, 1)
@@ -108,11 +121,11 @@ def clustering(rgb, obj_depth, img):
     plt.imshow(np.reshape(labels, [rows, cols]))
     plt.axis('off')
 
-    indices = np.dstack(np.indices(rgbd.shape[:2]))
-    rgbxyd = np.concatenate((rgbd, indices), axis=-1)
-    result = zip(np.reshape(rgbxyd, [-1, 6]), labels)
+    indices = np.dstack(np.indices(rgb.shape[:2]))
+    rgbxy = np.concatenate((rgb, indices), axis=-1)
+    result = zip(np.reshape(rgbxy, [-1, 5]), labels)
 
-    return result, labels
+    return result, set(labels)
 
 
 def biggest_cluster(result, clusters):
@@ -126,27 +139,10 @@ def biggest_cluster(result, clusters):
     size_dict = dict()
 
     for cluster in clusters:
-        num_relevant = len(filter(lambda res: res[1] == cluster, result))
+        num_relevant = len([res for res in result if res[1] == cluster])
         size_dict[cluster] = float(num_relevant)
 
     return size_dict
-
-
-def closest_cluster(result, clusters):
-    """
-    Identify the closest cluster based on image depth.
-    :param result: pixel-level features and cluster labels
-    :param clusters: set of identified clusters
-    :return: a dictionary with clusters as keys and their average depth as values
-    """
-    depth_dict = dict()
-
-    for cluster in clusters:
-        relevant = filter(lambda res: res[1] == cluster, result)
-        avg_depth = sum([res[0][3] for res in relevant]) / len(relevant)
-        depth_dict[cluster] = avg_depth
-
-    return depth_dict
 
 
 def middle_cluster(result, clusters):
@@ -159,21 +155,21 @@ def middle_cluster(result, clusters):
     position_dict = dict()
 
     for cluster in clusters:
-        relevant = filter(lambda res: res[1] == cluster, result)
+        relevant = [res for res in result if res[1] == cluster]
 
+        avg_3 = sum([res[0][3] for res in relevant]) / len(relevant)
         avg_4 = sum([res[0][4] for res in relevant]) / len(relevant)
-        avg_5 = sum([res[0][5] for res in relevant]) / len(relevant)
 
+        diff_3 = (0.5 * max([res[0][3] for res in result]) - avg_3) ** 2
         diff_4 = (0.5 * max([res[0][4] for res in result]) - avg_4) ** 2
-        diff_5 = (0.5 * max([res[0][5] for res in result]) - avg_5) ** 2
 
-        total_diff = diff_4 + diff_5
+        total_diff = diff_3 + diff_4
         position_dict[cluster] = total_diff
 
     return position_dict
 
 
-def normalize(dict_data):
+def normalize_dict(dict_data):
     """
     Map dictionary values to the [0, 1] interval.
     :param dict_data: a dictionary with numerical values
@@ -199,32 +195,18 @@ def dominant_cluster(result, clusters):
     """
 
     size = biggest_cluster(result, clusters)
-    depth = closest_cluster(result, clusters)
-    position = middle_cluster(result, clusters)
+    position_diff = middle_cluster(result, clusters)
 
-    normal_size = normalize(size)
-    normal_depth = normalize(depth)
-    normal_position = normalize(position)
+    normal_size = normalize_dict(size)
+    normal_position_diff = normalize_dict(position_diff)
 
-    biggest = max(normal_size, key=normal_size.get)
-    closest = min(normal_depth, key=normal_depth.get)
-    middle = min(normal_position, key=normal_position.get)
-
-    dom_list = [biggest, closest, middle]
-
+    cluster_dict = dict()
     for cluster in clusters:
+        cluster_dict[cluster] = normal_size[cluster] - normal_position_diff[cluster]
 
-        if dom_list.count(cluster) == 3:
-            dom_cluster = cluster
-            return dom_cluster
+    dom_cluster = max(cluster_dict, key=cluster_dict.get)
 
-        elif dom_list.count(cluster) == 2:
-            dom_cluster = cluster
-            return dom_cluster
-
-        else:
-            dom_cluster = closest
-            return dom_cluster
+    return dom_cluster
 
 
 def separate_rgb_channels(rgb_array):
@@ -243,7 +225,7 @@ def separate_rgb_channels(rgb_array):
 def manual_mode(a_list):
 
     max_count = max(a_list.count(item) for item in a_list)
-    all_max = filter(lambda v: a_list.count(v) == max_count, a_list)
+    all_max = [value for value in a_list if a_list.count(value) == max_count]
 
     return mean(all_max)
 
@@ -283,7 +265,7 @@ def color_mapping(avg_triplet, color_names):
     min_diff = 1000
     min_key = ''
 
-    for name, color_value in color_names.items():
+    for color_value, name in color_names.items():
         r_diff = (avg_triplet[0] - color_value[0]) ** 2
         g_diff = (avg_triplet[1] - color_value[1]) ** 2
         b_diff = (avg_triplet[2] - color_value[2]) ** 2
